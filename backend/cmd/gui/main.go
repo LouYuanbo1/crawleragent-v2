e92bf80171469e1589eccd26d0f1e190ba683e1a
LouYuanbo1 <louyuanbo1@gmail.com>
2026-01-04 02:23:30 +0800

    增加智能体配置页面和对应后端代码

--- backend/cmd/gui/main.go
+++ backend/cmd/gui/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"context"
 	"crawleragent-v2/internal/config"
-	controller "crawleragent-v2/internal/controller/document"
+	docController "crawleragent-v2/internal/controller/document"
+	searchAgentController "crawleragent-v2/internal/controller/searchagent"
+	"crawleragent-v2/internal/infra/embedding"
+	"crawleragent-v2/internal/infra/llm"
 	"crawleragent-v2/internal/infra/persistence/es"
+	service "crawleragent-v2/internal/service/searchagent"
+	"crawleragent-v2/param"
 	"fmt"
 	"log"
+	"time"
 
+	"github.com/cloudwego/eino-ext/components/tool/duckduckgo/v2"
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,15 +24,45 @@ func main() {
 		log.Fatalf("解析配置失败: %v", err)
 	}
 
+	ctx := context.Background()
+
 	router := gin.Default()
 
 	fmt.Printf("Chromedp UserDataDir: %s\n", appcfg.Rod.UserDataDir)
+
 	client, err := es.InitTypedEsClient(appcfg, 1)
 	if err != nil {
 		log.Fatalf("初始化ES客户端失败: %v", err)
 	}
-	docController := controller.InitDocumentController(client)
+
+	embedder, err := embedding.InitEmbedder(ctx, appcfg, 5, 1)
+	if err != nil {
+		log.Fatalf("初始化Embedder失败: %v", err)
+	}
+
+	llm, err := llm.InitLLM(ctx, appcfg)
+	if err != nil {
+		log.Fatalf("初始化LLM失败: %v", err)
+	}
+
+	paramSearchAgent := param.Agent{
+		DuckDuckGoSearch: param.SearchConfig{
+			MaxResults: 5,
+			Region:     duckduckgo.RegionCN,
+			Timeout:    20 * time.Second,
+		},
+	}
+
+	searchAgent, err := service.InitSearchAgentService(ctx, llm, client, embedder, &paramSearchAgent)
+	if err != nil {
+		log.Fatalf("初始化SearchAgentService失败: %v", err)
+	}
+
+	docController := docController.InitDocumentController(client)
 	docController.RegisterRoutes(router)
+
+	searchAgentController := searchAgentController.InitSearchAgentController(searchAgent)
+	searchAgentController.RegisterRoutes(router)
 
 	if err := router.Run(); err != nil {
 		log.Fatalf("failed to run server: %v", err)
--- backend/internal/controller/searchagent/searchagent_controller.go
+++ backend/internal/controller/searchagent/searchagent_controller.go
@@ -3,6 +3,7 @@ package controller
 import (
 	service "crawleragent-v2/internal/service/searchagent"
 	"crawleragent-v2/param"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
@@ -20,7 +21,7 @@ func InitSearchAgentController(searchAgentService service.SearchAgentService) *S
 func (sc *SearchAgentController) RegisterRoutes(r *gin.Engine) {
 	group := r.Group("/api/searchagent")
 	{
-		group.POST("/:index")
+		group.POST("", sc.Invoke)
 	}
 }
 
@@ -28,21 +29,18 @@ type StreamRequest struct {
 	param.QueryWithPrompt
 }
 
-func (sc *SearchAgentController) Stream(gctx *gin.Context) {
-	/*
-		index := gctx.Param("index")
-		var streamReq StreamRequest
-		if err := gctx.ShouldBindJSON(&streamReq); err != nil {
-			gctx.JSON(500, gin.H{"code": 500, "msg": fmt.Sprintf("invalid request: %s", err.Error()), "data": nil})
-			return
-		}
+func (sc *SearchAgentController) Invoke(gctx *gin.Context) {
+	var streamReq StreamRequest
+	if err := gctx.ShouldBindJSON(&streamReq); err != nil {
+		gctx.JSON(500, gin.H{"code": 500, "msg": fmt.Sprintf("invalid request: %s", err.Error()), "data": nil})
+		return
+	}
 
-			streamResp, err := sc.searchAgentService.Stream(gctx.Request.Context(), index, streamReq.QueryWithPrompt)
-			if err != nil {
-				gctx.JSON(500, gin.H{"code": 500, "msg": fmt.Sprintf("failed to stream: %s", err.Error()), "data": nil})
-				return
-			}
+	resp, err := sc.searchAgentService.Invoke(gctx.Request.Context(), &streamReq.QueryWithPrompt)
+	if err != nil {
+		gctx.JSON(500, gin.H{"code": 500, "msg": fmt.Sprintf("failed to stream: %s", err.Error()), "data": nil})
+		return
+	}
 
-		gctx.JSON(200, gin.H{"code": 200, "msg": "success", "data": streamResp})
-	*/
+	gctx.JSON(200, gin.H{"code": 200, "msg": "success", "data": resp})
 }
